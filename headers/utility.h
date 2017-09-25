cb508b324edf07eed481b177df7d9f5c2c65f3e9
Eugene Averyanov <euav@ya.ru>
2017-09-26 04:03:55 +0700

    Add new conversion functions

--- headers/utility.h
+++ headers/utility.h
@@ -6,14 +6,21 @@
 #include <random>
 #include "field.h"
 
+const double pi = 3.141592653589793;
+const double planck = 6.62607004081e-34;
+const double light_speed = 299792458;
+
 void relax_max(double& a, const double& b);
 void relax_mix(double& a, const double& b);
 double sinc(const double& x);
 
 double evm2_factor(const Field& tx, const Field& rx);
 double q2_factor(const Field& tx, const Field& rx);
-double dbm_to_watts(const double& power_dbm);
-void awgn_generator(Field& original, const double& variance);
+
+double db_to_linear(const double& db_value);
+double dbm_to_watts(const double& dbm_power);
+double db_to_natural(const double& db_value);
+double disp_to_beta2(const double& dispersion, const double& wavelength);
 
 Complex i_exp(const double& x);
 
--- sources/utility.cpp
+++ sources/utility.cpp
@@ -58,17 +58,20 @@ double q2_factor(const Field& tx, const Field& rx) {
     return 10 * std::log10(1 / evm2_factor(tx, rx));
 }
 
-double dbm_to_watts(const double& power_dbm) {
-    return 1e-3 * std::pow(10, power_dbm / 10);
+double db_to_linear(const double& db_value) {
+    return std::pow(10, db_value / 10);
 }
 
-void awgn_generator(Field& original, const double& variance) {
-    std::default_random_engine generator(time(0));
-    std::normal_distribution<double> noise(0, std::sqrt(variance / 2));
-    for (int i = 0; i < original.size(); ++i)
-        original[i] += Complex(noise(generator), noise(generator));
+double dbm_to_watts(const double& dbm_power) {
+    return 1e-3 * std::pow(10, dbm_power / 10);
 }
 
-Complex i_exp(const double& x) {
-    return Complex(std::cos(x), std::sin(x));
+double db_to_natural(const double& db_value) {
+    return db_value * std::log(10) / 10;
 }
+
+double disp_to_beta2(const double& dispersion, const double& wavelength) {
+    return -wavelength * wavelength * dispersion / (2 * pi * light_speed);
+}
+
+Complex i_exp(const double& x) { return Complex(std::cos(x), std::sin(x)); }
