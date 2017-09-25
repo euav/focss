b2e67dd9b4a023a8bd37a58e76e80b25a65e4377
Eugene Averyanov <euav@ya.ru>
2017-09-26 04:04:16 +0700

    Add all-include file

---
+++ headers/photonics.h
@@ -0,0 +1,13 @@
+#ifndef PHOTONICS_H_
+#define PHOTONICS_H_
+
+#include "field.h"
+#include "fiber.h"
+#include "ssfm.h"
+#include "edfa.h"
+#include "modulation.h"
+#include "lms_equalizer.h"
+#include "phase_shift_equalizer.h"
+#include "utility.h"
+
+#endif // PHOTONICS_H_
