cb7eafc4ef24aecedba5d92900c493f3b6b0e096
Eugene Averyanov <euav@ya.ru>
2017-09-26 04:05:31 +0700

    Rename EdfaAmplifier to EDFA and implement it

---
+++ headers/edfa.h
@@ -0,0 +1,26 @@
+#ifndef EDFA_H_
+#define EDFA_H_
+
+#include <random>
+#include "field.h"
+#include "utility.h"
+
+struct EDFA {
+    double gain = 1;
+    double noise_factor = 0;
+    double center_wavelength = 1.55e-6;
+    double bandwidth;
+
+    enum Scale { LINEAR, DECIBELS };
+
+  public:
+    EDFA(const double& gain, Scale = LINEAR);
+    EDFA(const double& db_gain,
+                  const double& noise_figure,
+                  Scale = LINEAR);
+
+    void amplify(Field& field) const;
+    void drop_power(Field& field) const;
+};
+
+#endif  // EDFA_H_
---
+++ sources/edfa.cpp
@@ -0,0 +1,33 @@
+#include "../headers/edfa.h"
+
+EDFA::EDFA(const double& gain, Scale scale) {
+    if (scale == LINEAR)
+        this->gain = gain;
+    else
+        this->gain = db_to_linear(gain);
+}
+
+EDFA::EDFA(const double& gain, const double& noise_factor, Scale scale) {
+    if (scale == LINEAR) {
+        this->gain = gain;
+        this->noise_factor = noise_factor;
+    } else {
+        this->gain = db_to_linear(gain);
+        this->noise_factor = db_to_linear(noise_factor);
+    }
+}
+
+void EDFA::amplify(Field& field) const {
+    field *= std::sqrt(gain);
+
+    double variance = 0.5 * noise_factor * (gain - 1);
+    variance *= planck * light_speed / center_wavelength;
+    variance *= field.getSamplingRate();
+
+    std::mt19937 generator(time(0));
+    std::normal_distribution<double> awgn(0, std::sqrt(variance / 2));
+    for (int i = 0; i < field.size(); ++i)
+        field[i] += Complex(awgn(generator), awgn(generator));
+}
+
+void EDFA::drop_power(Field& field) const { field *= std::sqrt(1 / gain); }
