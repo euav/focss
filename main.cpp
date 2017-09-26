8f9aab03ac8f9a6f88f0c9ce087d928cbe2378b7
Eugene Averyanov <euav@ya.ru>
2017-09-26 07:30:27 +0700

    use in experiment new classes

--- main.cpp
+++ main.cpp
@@ -1,134 +1,76 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include "headers/fiber.h"
-#include "headers/field.h"
-#include "headers/lms_equalizer.h"
-#include "headers/modulation.h"
-#include "headers/phase_shift_equalizer.h"
-#include "headers/utility.h"
+#include "headers/photonics.h"
 
 // reference units [m], [s], [W]
-const double bandwidth = 32e9;        // [baud]
-const double fiber_length = 1e5;      // [m]
-const double spans = 10;              // [1]
-const double noise_figure = 4.5;      // [dB]
-const double attenuation = 2e-4;      // [dB/m]
-const double dispersion = 1.7e-5;     // [s/m/m]
-const double wavelength = 1.55e-6;    // [m]
-const double nonlinearity = 1.4e-3;   // [1/W/m]
-const double rrc_roll_off = 0.01;     // [1]
-const int pulse_width = 1024;         // [1]
-const int oversampling = 16;          // [1]
-const double max_phase_shift = 1e-3;  // [1]
-const double max_forward_step = 1e3;  // [m]
-const int training_length = 4096;     // [1]
+const double attenuation = 2e-4;        // [dB/m]
+const double dispersion = 1.7e-5;       // [s/m/m]
+const double nonlinearity = 1.4e-3;     // [1/W/m]
+const double wavelength = 1.55e-6;      // [m]
+const double fiber_length = 1e5;        // [m]
+const double pol_coupling = 8.0 / 9.0;  // [1]
+const double bandwidth = 32e9;          // [baud]
+const double noise_figure = 4.5;        // [dB]
+const double spans = 10;                // [1]
+const double max_phase_shift = 1e-3;    // [1]
+const double max_forward_step = 1e3;    // [m]
+const int training_length = 256;        // [1]
 const int IDEAL = 0;
-
-void lowpass_inplace(Field& field, const double& cutoff_frequency) {
-    double time_domain = field.size() / field.getSamplingRate();
-    int cutoff_index = int(cutoff_frequency * time_domain);
-    std::cout << "+ cutoff: " << cutoff_index << "\n";
-
-    field.fft_inplace();
-    for (int i = cutoff_index + 1; i < field.size() - cutoff_index; ++i)
-        field[i] = 0;
-    field.ifft_inplace();
-}
-
-Field random_16qam_symbols(const int& length) {
-    Field symbols(length);
-    for (int i = 0; i < length; ++i)
-        symbols[i] = gray_symbols_16qam[i % 16];
-
-    std::shuffle(symbols.begin(), symbols.end(), std::mt19937(time(0)));
-    return symbols;
-}
-
-Field modulate_rrc(const Field& symbols, const double& launch_power) {
-    Field rrc = rrc_filter(rrc_roll_off, pulse_width, oversampling);
-    Field signal = symbols.upsample(oversampling);
-    signal.apply_filter(rrc);
-    signal *= std::sqrt(launch_power / signal.average_power());
-    signal.setSampligRate(bandwidth * oversampling);
-
-    return signal;
-}
-
-Field demodulate_rrc(const Field& signal, const int& osf) {
-    Field symbols = signal;
-    Field rrc = rrc_filter(rrc_roll_off, pulse_width, osf);
-    symbols = symbols.apply_filter(rrc).downsample(osf);
-    symbols *= std::sqrt(1.0 / symbols.average_power());
-
-    return symbols;
-}
+const Fiber fiber = {db_to_natural(attenuation),
+                     disp_to_beta2(dispersion, wavelength),
+                     nonlinearity,
+                     fiber_length,
+                     wavelength,
+                     pol_coupling};
 
 void forward_propagation(Field& signal) {
     std::cout << ">> forward propagation" << std::endl;
 
-    double noise_variance = 0.5 * pow(10, noise_figure / 10);
-    noise_variance *= pow(10, attenuation * fiber_length / 10) - 1;
-    noise_variance *= planck * light_speed / wavelength;
-    noise_variance *= signal.getSamplingRate();
-
-    Fiber fiber(wavelength);
-    fiber.setAttenuationDB(attenuation);
-    fiber.setDispersionPhysical(dispersion);
-    fiber.setNonlinearity(nonlinearity);
-    fiber.setFiberLength(fiber_length);
-    fiber.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
+    SSFM ssfm(fiber);
+    ssfm.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
+    EDFA edfa(attenuation * fiber_length, noise_figure, EDFA::DECIBELS);
     for (int i = 0; i < spans; ++i) {
-        fiber.propagate(signal);
-        fiber.amplify(signal);
-        awgn_generator(signal, noise_variance);
+        ssfm.run(signal);
+        edfa.amplify(signal);
         std::cout << "    * span " << i + 1 << " has done" << std::endl;
     }
     std::cout << std::endl;
 }
 
 void cd_compensation(Field& signal) {
-    Fiber fiber(wavelength);
-    fiber.setAttenuationDB(attenuation);
-    fiber.setDispersionPhysical(dispersion);
-    fiber.setNonlinearity(nonlinearity);
-    fiber.setFiberLength(fiber_length);
-    fiber.compensateCD(signal, spans);
+    SSFM ssfm(-fiber.dispersive() * spans);
+    ssfm.setTotalSteps(1);
+    ssfm.run(signal);
 }
 
 void backward_propagation(Field& signal, const int& steps = 0) {
-    Fiber fiber(wavelength);
-    fiber.setAttenuationDB(-attenuation);
-    fiber.setDispersionPhysical(-dispersion);
-    fiber.setNonlinearity(-nonlinearity);
-    fiber.setFiberLength(fiber_length);
+    SSFM ssfm(-fiber);
+    EDFA edfa(attenuation * fiber_length, noise_figure, EDFA::DECIBELS);
+
     if (steps > 0)
-        fiber.setTotalSteps(steps);
+        ssfm.setTotalSteps(steps);
     else
-        fiber.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
+        ssfm.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
 
     for (int i = 0; i < spans; ++i) {
-        fiber.amplify(signal);
-        fiber.propagate(signal);
+        edfa.drop_power(signal);
+        ssfm.run(signal);
     }
 }
 
-void experiment(const int& symbols_length,
-                const double& average_power_dbm,
-                std::ostream& output_stream) {
+void final_experiment(const int& symbols_length,
+                      const double& average_power_dbm,
+                      std::ostream& output_stream) {
     output_stream << average_power_dbm << ',';
 
     // propagation
-    Field symbols = random_16qam_symbols(symbols_length);
-    Field signal = modulate_rrc(symbols, dbm_to_watts(average_power_dbm));
+    Field symbols = random_16qam_symbols(symbols_length, bandwidth);
+    Field signal = rrc_modulate(symbols, 16, dbm_to_watts(average_power_dbm));
     forward_propagation(signal);
 
     std::cout << ">> experimenting with dbp" << std::endl;
     for (int i = 1; i <= 11; ++i) {
-        Field dbp_signal = signal;
-        lowpass_inplace(dbp_signal, 0.5 * bandwidth);
-        dbp_signal = dbp_signal.downsample(8);
+        Field dbp_signal = signal.decimate(8);
 
         // sps parameter
         if (i <= 10)
@@ -136,11 +78,11 @@ void experiment(const int& symbols_length,
         else
             backward_propagation(dbp_signal, IDEAL);
 
-        dbp_signal = demodulate_rrc(dbp_signal, 2);
+        dbp_signal = rrc_demodulate(dbp_signal, 2);
 
         Field train_x = symbols.chomp(0, symbols_length - training_length);
         Field train_y = dbp_signal.chomp(0, symbols_length - training_length);
-        PhaseShiftEqualizer pse(720);
+        PhaseShiftEqualizer pse(360);
         pse.train(train_x, train_y);
         Field valid_x = symbols.chomp(training_length, 0);
         Field valid_y = pse.equalize(dbp_signal).chomp(training_length, 0);
@@ -151,12 +93,16 @@ void experiment(const int& symbols_length,
     std::cout << ">> experimenting with lms and pse" << std::endl;
     {
         Field cd_signal = signal;
-        cd_compensation(cd_signal);
-        cd_signal = demodulate_rrc(cd_signal, oversampling);
+
+        SSFM ssfm(-fiber.dispersive() * spans);
+        ssfm.setTotalSteps(1);
+        ssfm.run(cd_signal);
+
+        cd_signal = rrc_demodulate(cd_signal, 16);
 
         Field train_x = symbols.chomp(0, symbols_length - training_length);
         Field train_y = cd_signal.chomp(0, symbols_length - training_length);
-        PhaseShiftEqualizer pse(720);
+        PhaseShiftEqualizer pse(360);
         LmsEqualizer lms(3);
 
         pse.train(train_x, train_y);
@@ -173,16 +119,16 @@ void experiment(const int& symbols_length,
 }
 
 int main() {
-    std::ofstream fout("experiment/right.txt");
-    fout << "#AVG(dBm),DBP(1-10,ideal),PSE,LMS(3)\n";
+    std::ofstream fout("experiment/final.txt");
+    fout << "#AVG(dBm),DBP(1-10,ideal),PSE,LMS(3)" << std::endl;
     fout.precision(15);
 
-    for (int i = 31; i <= 40; ++i) {
+    for (int i = 0; i <= 40; ++i) {
         double average_power_dbm = double(i) / 2.0 - 10.0;
         std::cout << "\nstarting experiment at power ";
         std::cout << average_power_dbm << " dBm" << std::endl;
 
-        experiment(16384, average_power_dbm, fout);
+        final_experiment(1024, average_power_dbm, fout);
     }
     fout.close();
 
