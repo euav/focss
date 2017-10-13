be0217d9134caf2534f862cd399ce6989ff3eb6d
Eugene Averyanov <euav@ya.ru>
2017-10-13 07:12:59 +0700

    change experiments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <iostream>
-#include "focss/focss.h"
 #include "focss/equalizer/nonlinear_phase_recovery.h"
 #include "focss/equalizer/sino.h"
+#include "focss/focss.h"
 
 // reference units [m], [s], [W]
 const double attenuation = 2e-4;        // [dB/m]
@@ -16,7 +16,7 @@ const double noise_figure = 4.5;        // [dB]
 const double spans = 10;                // [1]
 const double max_phase_shift = 1e-2;    // [1]
 const double max_forward_step = 1e3;    // [m]
-const int training_length = 1536;        // [1]
+const int training_percent = 75;        // [1]
 const int IDEAL = 0;
 const Fiber fiber = {db_to_natural(attenuation),
                      disp_to_beta2(dispersion, wavelength),
@@ -30,10 +30,10 @@ void forward_propagation(Field& signal) {
 
     SSFM ssfm(fiber);
     ssfm.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
-    EDFA edfa(attenuation * fiber_length, noise_figure, EDFA::DECIBELS);
+    Amplifier Amplifier(attenuation * fiber_length, noise_figure, Amplifier::DECIBELS);
     for (int i = 0; i < spans; ++i) {
         ssfm.run(signal);
-        edfa.amplify(signal);
+        Amplifier.amplify(signal);
         std::cout << "    * span " << i + 1 << " has done" << std::endl;
     }
     std::cout << std::endl;
@@ -47,7 +47,7 @@ void cd_compensation(Field& signal) {
 
 void backward_propagation(Field& signal, const int& steps = 0) {
     SSFM ssfm(-fiber);
-    EDFA edfa(attenuation * fiber_length, noise_figure, EDFA::DECIBELS);
+    Amplifier Amplifier(attenuation * fiber_length, noise_figure, Amplifier::DECIBELS);
 
     if (steps > 0)
         ssfm.setTotalSteps(steps);
@@ -55,12 +55,12 @@ void backward_propagation(Field& signal, const int& steps = 0) {
         ssfm.setMaximumShiftAndStep(max_phase_shift, max_forward_step);
 
     for (int i = 0; i < spans; ++i) {
-        edfa.drop_power(signal);
+        Amplifier.drop_power(signal);
         ssfm.run(signal);
     }
 }
 
-void final_experiment(const int& symbols_length,
+void final_experiment(const unsigned long& symbols_length,
                       const double& average_power_dbm,
                       std::ostream& output_stream) {
     output_stream << average_power_dbm << ',';
@@ -82,6 +82,7 @@ void final_experiment(const int& symbols_length,
 
         dbp_signal = rrc_demodulate(dbp_signal, 2);
 
+        unsigned long training_length = training_percent * symbols_length / 100;
         Field train_x = symbols.chomp(0, symbols_length - training_length);
         Field train_y = dbp_signal.chomp(0, symbols_length - training_length);
         IdealPhaseRecovery pse(360);
@@ -102,6 +103,7 @@ void final_experiment(const int& symbols_length,
 
         cd_signal = rrc_demodulate(cd_signal, 16);
 
+        unsigned long training_length = training_percent * symbols_length / 100;
         Field train_x = symbols.chomp(0, symbols_length - training_length);
         Field train_y = cd_signal.chomp(0, symbols_length - training_length);
         IdealPhaseRecovery pse(360);
@@ -120,9 +122,9 @@ void final_experiment(const int& symbols_length,
     std::cout << ">> experiment has done" << std::endl;
 }
 
-void nlpr_experiment(const int& symbols_length,
-                      const double& average_power_dbm,
-                      std::ostream& output_stream) {
+void sino_experiment(const unsigned long& symbols_length,
+                     const double& average_power_dbm,
+                     std::ostream& output_stream) {
     output_stream << average_power_dbm << ',';
 
     // propagation
@@ -137,92 +139,98 @@ void nlpr_experiment(const int& symbols_length,
     ssfm.run(cd_signal);
     cd_signal = rrc_demodulate(cd_signal, 16);
 
-    std::cout << ">> experimenting with lms, ipr and nlpr" << std::endl;
-    {
+    // digital back propagation
+    Field dbp_signal = signal.decimate(8);
+    backward_propagation(dbp_signal, 1);
+    dbp_signal = rrc_demodulate(dbp_signal, 2);
+    
+    std::cout << ">> experimenting with linear eqs and sino" << std::endl;
+        unsigned long training_length = training_percent * symbols_length / 100;
         Field train_x = symbols.chomp(0, symbols_length - training_length);
         Field train_y = cd_signal.chomp(0, symbols_length - training_length);
 
-        LeastMeanSquare lms(3);
+        LeastMeanSquare lms(1);
         IdealPhaseRecovery ipr(360);
-        NonlinearPhaseRecovery nlpr3(1);
-        NonlinearPhaseRecovery nlpr7(3);
-        NonlinearPhaseRecovery nlpr11(5);
-        NonlinearPhaseRecovery nlpr15(7);
-        NonlinearPhaseRecovery nlpr27(13);
-        NonlinearPhaseRecovery nlpr41(20);
-        SINO sino1(1);
-        SINO sino2(2);
-        SINO sino3(3);
+        NonlinearPhaseRecovery nlpr;
+        SINO sino(13, 0.4);
 
         lms.train(train_x, train_y);
         ipr.train(train_x, train_y);
-        nlpr3.train(train_x, train_y);
-        nlpr7.train(train_x, train_y);
-        nlpr11.train(train_x, train_y);
-        nlpr15.train(train_x, train_y);
-        nlpr27.train(train_x, train_y);
-        nlpr41.train(train_x, train_y);
-        sino1.train(train_x, train_y);
-        sino2.train(train_x, train_y);
-        sino3.train(train_x, train_y);
+        nlpr.train(train_x, train_y);
+        sino.train(train_x, train_y);
 
-        Field valid_lms = lms.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_ipr = ipr.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_nlpr7 = nlpr7.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_nlpr11 = nlpr11.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_nlpr15 = nlpr15.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_nlpr27 = nlpr27.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_nlpr41 = nlpr41.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_sino1 = sino1.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_sino2 = sino2.equalize(cd_signal).chomp(training_length, 20);
-        Field valid_sino3 = sino3.equalize(cd_signal).chomp(training_length, 20);
-     
-        Field valid_nlpr_lms;
-        {
-            Field train_y_nlpr = nlpr27.equalize(train_y).chomp(13, 13);
-            lms.train(train_x.chomp(13, 13), train_y_nlpr);
-            valid_nlpr_lms = lms.equalize(cd_signal).chomp(training_length, 20);
-        }
-
-        Field valid_lms_nlpr;
-        {
-            lms.train(train_x, train_y);
-            Field train_y_lms = lms.equalize(train_y).chomp(2, 0);
-            nlpr27.train(train_x.chomp(2, 0), train_y_lms);
-            valid_lms_nlpr = nlpr27.equalize(cd_signal).chomp(training_length, 20);
-        }
-        
-        Field valid_x = symbols.chomp(training_length, 20);
+        int cut = 30;
+        Field valid_x = symbols.chomp(training_length, cut);
+        Field valid_lms = lms.equalize(cd_signal).chomp(training_length, cut);
+        Field valid_ipr = ipr.equalize(cd_signal).chomp(training_length, cut);
+        Field valid_nlpr = nlpr.equalize(cd_signal).chomp(training_length, cut);
+        Field valid_sino = sino.equalize(cd_signal).chomp(training_length, cut);
 
         output_stream << q2_factor(valid_x, valid_lms) << ',';
         output_stream << q2_factor(valid_x, valid_ipr) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr7) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr11) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr15) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr27) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr41) << ',';
-        output_stream << q2_factor(valid_x, valid_lms_nlpr) << ',';
-        output_stream << q2_factor(valid_x, valid_nlpr_lms) << ',';
-        output_stream << q2_factor(valid_x, valid_sino1) << ',';
-        output_stream << q2_factor(valid_x, valid_sino2) << ',';
-        output_stream << q2_factor(valid_x, valid_sino3) << std::endl;
+        output_stream << q2_factor(valid_x, valid_nlpr) << ',';
+        output_stream << q2_factor(valid_x, valid_sino) << std::endl;
+
+        ComplexVector w = sino.getWeights();
+        std::ofstream mat("mat_re.txt");
+        unsigned long length = 2 * 13 + 1;
+        for (unsigned long i = 0; i < length; ++i) {
+            for (unsigned long j = 0; j < length; ++j) {
+                mat << std::sqrt(norm(w[i * length + j])) << ' ';
+            }
+            mat << '\n';
+        }
+
+    std::cout << ">> saving results" << std::endl;
+    std::ofstream cons("constellations.csv");
+    for (int i = 0; i < valid_x.size(); ++i) {
+        cons << valid_lms[i].real() << ',' << valid_lms[i].imag() << ',';
+        cons << valid_sino[i].real() << ',' << valid_sino[i].imag() << ',';
+        cons << dbp_signal[i].real() << ',' << dbp_signal[i].imag() << '\n';
     }
-    std::cout << ">> experiment has done" << std::endl;
+    cons.close();
+}
+
+void save_transmission(const unsigned long& symbols_length,
+                       const double& average_power_dbm,
+                       std::ostream& output_stream) {
+    // propagation
+    Field symbols = random_16qam_symbols(symbols_length, bandwidth);
+    Field signal = rrc_modulate(symbols, 16, dbm_to_watts(average_power_dbm));
+    forward_propagation(signal);
+
+    // cd compensation
+    Field cd_signal = signal;
+    SSFM ssfm(-fiber.dispersive() * spans);
+    ssfm.setTotalSteps(1);
+    ssfm.run(cd_signal);
+    cd_signal = rrc_demodulate(cd_signal, 16);
+
+    std::cout << ">> saving to file" << std::endl;
+    for (unsigned long i = 0; i < symbols_length; ++i) {
+        output_stream << symbols[i].real() << ',' << symbols[i].imag() << ',';
+        output_stream << cd_signal[i].real() << ',' << cd_signal[i].imag()
+                      << '\n';
+    }
 }
 
 int main() {
-    std::ofstream fout("nlpr.txt");
-    fout << "#AVG(dBm),LMS(3),IPR,NLPR(7,11,15,27,41),LMS-NLPR,NLPR-LMS,SINO(1),SINO(2),SINO(3)" << std::endl;
+    std::ofstream fout("sino11.txt");
+    fout << "# P[dBm], DBP(1), LMS(3), IPR, NLPR, SINO(11)" << std::endl;
     fout.precision(15);
+    std::cout.precision(15);
 
-    for (int i = 0; i <= 40; ++i) {
-        double average_power_dbm = double(i) / 2.0 - 10.0;
-        std::cout << "\nstarting experiment at power ";
-        std::cout << average_power_dbm << " dBm" << std::endl;
+    // for (int i = 0; i <= 40; ++i) {
+    //     double average_power_dbm = double(i) / 2.0 - 10.0;
+    //     std::cout << "\nstarting experiment at power ";
+    //     std::cout << average_power_dbm << " dBm" << std::endl;
 
-        nlpr_experiment(2048, average_power_dbm, fout);
-    }
+    //     sino_experiment(4096, average_power_dbm, fout);
+    // }
+    sino_experiment(16384, -1, std::cout);
+
     fout.close();
+
 
     return 0;
 }
