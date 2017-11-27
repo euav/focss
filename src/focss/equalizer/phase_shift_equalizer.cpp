421cc6ca4591a88ddf8af9d772a923ce947bdfb9
Eugene Averyanov <euav@ya.ru>
2017-11-27 15:55:45 +0700

    make PSE works with Field and ComplexVector

--- src/focss/equalizer/phase_shift_equalizer.cpp
+++ src/focss/equalizer/phase_shift_equalizer.cpp
@@ -2,13 +2,14 @@
 #include "focss/functions.h"
 
 namespace focss {
-PhaseShiftEqualizer::PhaseShiftEqualizer() : steps(360), estimated_angle(0) {}
+PhaseShiftEqualizer::PhaseShiftEqualizer()
+    : angle_steps_(360), estimated_angle_(0) {}
 
 PhaseShiftEqualizer::PhaseShiftEqualizer(const int& angle_steps)
-    : steps(angle_steps), estimated_angle(0) {}
+    : angle_steps_(angle_steps), estimated_angle_(0) {}
 
-void PhaseShiftEqualizer::setAngleSteps(const int& angle_steps) {
-    steps = angle_steps;
+void PhaseShiftEqualizer::set_angle_steps(const int& angle_steps) {
+    angle_steps_ = angle_steps;
 }
 
 void PhaseShiftEqualizer::train(const ComplexVector& desired,
@@ -16,8 +17,8 @@ void PhaseShiftEqualizer::train(const ComplexVector& desired,
     double angle, q2;
     double argmax_angle = 0;
     double max_q2 = q2_factor(desired, actual);
-    for (int i = 0; i < steps; ++i) {
-        angle = 2.0 * M_PI * double(i) / steps;
+    for (int i = 0; i < angle_steps_; ++i) {
+        angle = 2 * math_pi * double(i) / angle_steps_;
         q2 = q2_factor(desired, actual * i_exp(angle));
 
         if (max_q2 < q2) {
@@ -26,13 +27,34 @@ void PhaseShiftEqualizer::train(const ComplexVector& desired,
         }
     }
 
-    estimated_angle = argmax_angle;
+    estimated_angle_ = argmax_angle;
 }
 
-double PhaseShiftEqualizer::getAngle() const { return estimated_angle; }
+void PhaseShiftEqualizer::train(const Field& desired, const Field& actual) {
+    double angle, q2;
+    double argmax_angle = 0;
+    double max_q2 = q2_factor(desired, actual);
+    for (int i = 0; i < angle_steps_; ++i) {
+        angle = 2 * math_pi * double(i) / angle_steps_;
+        q2 = q2_factor(desired, actual * i_exp(angle));
+
+        if (max_q2 < q2) {
+            max_q2 = q2;
+            argmax_angle = angle;
+        }
+    }
+
+    estimated_angle_ = argmax_angle;
+}
+
+double PhaseShiftEqualizer::get_angle() const { return estimated_angle_; }
+
+Field PhaseShiftEqualizer::equalize(const Field& original) const {
+    return original * i_exp(estimated_angle_);
+}
 
 ComplexVector PhaseShiftEqualizer::equalize(
     const ComplexVector& original) const {
-    return original * i_exp(estimated_angle);
+    return original * i_exp(estimated_angle_);
 }
 }  // namespace focss
