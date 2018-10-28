eeeaf4d9f039828f6c4453d77bd6dff751146776
Eugene Averyanov <euav@ya.ru>
2018-10-29 05:55:06 +0700

    rename numeric types

--- src/focss/definitions.h
+++ src/focss/definitions.h
@@ -1,18 +1,19 @@
-#ifndef FOCSS_CORE_H_
-#define FOCSS_CORE_H_
+#ifndef FOCSS_DEFINITIONS_H_
+#define FOCSS_DEFINITIONS_H_
 
 #include <complex>
 #include "focss/vector.h"
 
 namespace focss {
-const double math_pi = 3.14159265358979323846264338;  // [1]
-const double planck = 6.62607004081818181818181e-34;  // [m^2 kg / s]
-const double light_speed = 299792458;                 // [m/s]
+typedef double real_t;
+typedef std::complex<real_t> complex_t;
+typedef Vector<real_t> RealVector;
+typedef Vector<complex_t> ComplexVector;
 
-typedef std::complex<double> Complex;
-typedef Vector<Complex> ComplexVector;
-typedef Vector<double> RealVector;
-const Complex i_unit = Complex(0, 1);
+const real_t math_pi = 3.14159265358979323846264338;  // [1]
+const real_t planck = 6.62607004081818181818181e-34;  // [m^2 kg / s]
+const real_t light_speed = 299792458;                 // [m / s]
+const complex_t i_unit = complex_t(0, 1);
 }  // namespace focss
 
-#endif  // FOCSS_CORE_H_
+#endif  // FOCSS_DEFINITIONS_H_
