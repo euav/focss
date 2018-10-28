0a58ddaa98cc8bc3321d50b28477d94e93c1bd87
Eugene Averyanov <euav@ya.ru>
2018-10-29 05:52:42 +0700

    add more complicated features

--- src/focss/equalizer/sino.cpp
+++ src/focss/equalizer/sino.cpp
@@ -1,88 +1,262 @@
-#include "focss/equalizer/sino.h"
-#include "lasso_admm.h"
-#include <armadillo>
+#include "sino.h"
+#include <cassert>
+#include <iostream>
+#include "admm_regression.h"
 
-SINO::SINO() : radius(0), lambda(1), trained(false) {}
+namespace focss {
+SINO::SINO() : radius_(0), lambda_(1), type_(ORDINARY), trained_(false) {}
 
-SINO::SINO(const unsigned long& symbol_radius)
-    : radius(symbol_radius), lambda(1), trained(false) {}
+SINO::SINO(const int& radius)
+    : radius_(radius), lambda_(1), type_(ORDINARY), trained_(false) {}
 
-SINO::SINO(const unsigned long& symbol_radius, const double& parameter)
-    : radius(symbol_radius), lambda(parameter), trained(false) {}
+SINO::SINO(const int& radius, const double& parameter)
+    : radius_(radius), lambda_(parameter), type_(ORDINARY), trained_(false) {}
 
-void SINO::setSymbolRadius(const unsigned long& symbol_radius) {
-    radius = symbol_radius;
-    trained = false;
+SINO::SINO(const int& radius, const double& parameter, const SinoType& type)
+    : radius_(radius), lambda_(parameter), type_(type), trained_(false) {}
+
+SINO& SINO::set_radius(const int& radius) {
+    radius_ = radius;
+    trained_ = false;
+    return *this;
 }
 
-void SINO::setRegularizationParameter(const double& parameter) {
-    lambda = parameter;
-    trained = false;
+SINO& SINO::set_lambda(const double& lambda) {
+    lambda_ = lambda;
+    trained_ = false;
+    return *this;
 }
 
-void SINO::train(const Field& desired, const Field& actual) {
-    if (desired.size() != actual.size() || actual.size() < 4 * radius + 1)
-        return;
+SINO& SINO::set_type(const SinoType& type) {
+    type_ = type;
+    trained_ = false;
+    return *this;
+}
 
-    unsigned long n_samples = actual.size() - 4 * radius;
-    unsigned long n_features = (2 * radius + 1) * (2 * radius + 1);
-    arma::cx_mat features(n_samples, n_features + 1);
-    arma::cx_vec responses = desired.chomp(2 * radius, 2 * radius);
+SINO& SINO::set_linear_weight(const complex_t& linear_weight) {
+    linear_weight_ = linear_weight;
+    trained_ = false;
+    return *this;
+}
 
-    for (unsigned long i = 2 * radius; i < actual.size() - 2 * radius; ++i) {
-        for (unsigned long j = 0; j < n_features; ++j) {
-            features(i - 2 * radius, j) = term(actual, i, j);
-        }
-        features(i - 2 * radius, n_features) = actual[i];
+void SINO::train(const Field& transmitted, const Field& received) {
+    assert(transmitted.domain() == received.domain());
+    using namespace arma;
+
+    int n_samples_per_mode = transmitted.samples() - 2 * radius_;
+    int n_modes = transmitted.modes();
+    int n_samples = n_samples_per_mode * n_modes;
+    int n_features = radius_ * (radius_ + 2) / 2 + 1;
+    if (type_ == ORDINARY) n_features++;
+    int shift = radius_;
+
+    cx_vec responses(n_samples);
+    if (type_ == ORDINARY || type_ == HOMOGENEOUS) {
+        for (int mode = 0; mode < n_modes; ++mode)
+            for (int sample = 0; sample < n_samples_per_mode; ++sample)
+                responses(sample + mode * n_samples_per_mode) =
+                    received(mode, sample + shift);
+    } else {  // type_ == FIXED
+        for (int mode = 0; mode < n_modes; ++mode)
+            for (int sample = 0; sample < n_samples_per_mode; ++sample)
+                responses(sample + mode * n_samples_per_mode) =
+                    received(mode, sample + shift) -
+                    transmitted(mode, sample + shift) * linear_weight_;
     }
 
-    arma::vec x_stddev = stddev(features, 1, 0).st();
-    arma::cx_vec x_mean = mean(features, 0).st();
-    Complex y_mean = mean(responses);
+    cx_mat features(n_samples, n_features, fill::zeros);
+    for (int mode = 0; mode < n_modes; ++mode) {
+        for (int sample = 0; sample < n_samples_per_mode; ++sample) {
+            int index = 0;
+
+            int m_border = radius_ / 2;
+            for (int m = -m_border; m <= m_border; ++m) {
+                int n_left = std::abs(m);
+                int n_right = radius_ - std::abs(m);
+
+                for (int n = n_left; n <= n_right; ++n) {
+                    if (m == 0 && n == 0) {
+                        features(sample + mode * n_samples_per_mode, index++) =
+                            term(transmitted, mode, sample + shift, 0, 0);
+                    } else if (m == n) {
+                        features(sample + mode * n_samples_per_mode, index++) =
+                            term(transmitted, mode, sample + shift, m, n) +
+                            term(transmitted, mode, sample + shift, -m, -n);
+                    } else if (m == -n) {
+                        features(sample + mode * n_samples_per_mode, index++) =
+                            term(transmitted, mode, sample + shift, m, n) +
+                            term(transmitted, mode, sample + shift, n, m);
+                    } else {
+                        features(sample + mode * n_samples_per_mode, index++) =
+                            term(transmitted, mode, sample + shift, m, n) +
+                            term(transmitted, mode, sample + shift, n, m) +
+                            term(transmitted, mode, sample + shift, -m, -n) +
+                            term(transmitted, mode, sample + shift, -n, -m);
+                    }
+                }
+            }
+
+            if (type_ == ORDINARY)
+                features(sample + mode * n_samples_per_mode, index) =
+                    transmitted(mode, sample + shift);
+        }
+    }
+
+    vec x_stddev = stddev(features, 1, 0).st();
+    cx_vec x_mean = mean(features, 0).st();
+    complex_t y_mean = mean(responses);
     responses = responses - y_mean;
 
-    for (unsigned long i = 0; i < n_samples; ++i)
-        for (unsigned long j = 0; j < n_features + 1; ++j)
+    for (uword i = 0; i < n_samples; ++i)
+        for (uword j = 0; j < n_features; ++j)
             features(i, j) = (features(i, j) - x_mean(j)) / x_stddev(j);
 
-    arma::cx_vec raw_weights = cx_lasso_admm(features, responses, lambda);
+    cx_vec raw_weights = solve(features, responses);
+    // cx_vec raw_weights = cx_lad_admm(features, responses);
+    // cx_vec raw_weights = cx_ridge_cholesky(features, responses, lambda_);
+    // cx_vec raw_weights = cx_sag(features, responses, lambda_);
+    // cx_vec raw_weights = cx_modulus_cholesky(features, responses,
+    // x_stddev(n_features - 1));
+    std::cout << arma::norm(features * raw_weights - responses) << std::endl;
+
     raw_weights = raw_weights / x_stddev;
-    intercept = y_mean - sum(raw_weights % x_mean / x_stddev);
+    intercept_ = y_mean - sum(raw_weights % x_mean);
+    weights_ = cx_mat(2 * radius_ + 1, 2 * radius_ + 1, fill::zeros);
 
-    weights = arma::conv_to<ComplexVector>::from(raw_weights);
-    trained = true;
+    int index = 0;
+    int m_border = radius_ / 2;
+    for (int m = -m_border; m <= m_border; ++m) {
+        int n_left = std::abs(m);
+        int n_right = radius_ - std::abs(m);
+
+        for (int n = n_left; n <= n_right; ++n) {
+            if (m == 0 && n == 0) {
+                weights_(radius_, radius_) = raw_weights(index++);
+            } else if (m == n) {
+                weights_(radius_ + m, radius_ + n) = raw_weights(index);
+                weights_(radius_ - m, radius_ - n) = raw_weights(index++);
+            } else if (m == -n) {
+                weights_(radius_ + m, radius_ + n) = raw_weights(index);
+                weights_(radius_ + n, radius_ + m) = raw_weights(index++);
+            } else {
+                weights_(radius_ + m, radius_ + n) = raw_weights(index);
+                weights_(radius_ + n, radius_ + m) = raw_weights(index);
+                weights_(radius_ - m, radius_ - n) = raw_weights(index);
+                weights_(radius_ - n, radius_ - m) = raw_weights(index++);
+            }
+        }
+    }
+
+    if (type_ == ORDINARY)
+        linear_weight_ = raw_weights(index);
+    else if (type_ == HOMOGENEOUS)
+        linear_weight_ = 0.0;
+
+    trained_ = true;
+    std::cout << "> sino has been trained (";
+    std::cout << radius_ << ")" << std::endl;
+    save_matrix("data/mmm.csv");
 }
 
-Complex SINO::term(const Field& symbols,
-                   const unsigned long& center_index,
-                   const unsigned long& term_index) const {
-    unsigned long n = term_index % (2 * radius + 1) - radius;
-    unsigned long m = term_index / (2 * radius + 1) - radius;
+complex_t SINO::term(const Field& field,
+                     const int& mode,
+                     const int& k,
+                     const int& m,
+                     const int& n) {
+    complex_t term = 0.0;
+    for (int p = 0; p < field.modes(); ++p)
+        term += field(p, k + n) * conj(field(p, k + m + n));
 
-    Complex term = symbols[center_index + n];
-    term *= symbols[center_index + m];
-    term *= conj(symbols[center_index + n + m]);
-
-    return term;
+    return field(mode, k + m) * term;
 }
 
-ComplexVector SINO::getWeights() const {
-    if (trained)
-        return weights;
-    else
-        return ComplexVector(1, 1);
+complex_t SINO::get_intercept() const {
+    assert(trained_);
+    return intercept_;
 }
 
-Field SINO::equalize(const Field& original) const {
-    if (!trained || original.size() < 4 * radius + 1) return original;
-    unsigned long n_features = (2 * radius + 1) * (2 * radius + 1);
+complex_t SINO::get_linear_weight() const {
+    assert(trained_);
+    return linear_weight_;
+}
 
-    Field equalized(original.size());
-    for (unsigned long i = 2 * radius; i < original.size() - 2 * radius; ++i) {
-        equalized[i] = weights[n_features] * original[i];
-        for (unsigned long j = 0; j < n_features; ++j)
-            equalized[i] += weights[j] * term(original, i, j);
+complex_t SINO::get_coo_weight() const {
+    assert(trained_);
+    return weights_(radius_, radius_);
+}
+
+Field SINO::model(const Field& original) const {
+    assert(trained_);
+    return linear(original) + cubic(original);
+}
+
+Field SINO::equalize(const Field& rx, const Field& tx) const {
+    assert(trained_);
+    return (rx - cubic(tx)) / linear_weight_;
+}
+
+Field SINO::linear(const Field& original) const {
+    assert(trained_);
+    return linear_weight_ * original;
+}
+
+Field SINO::cubic(const Field& original) const {
+    assert(trained_);
+
+    Field equalized(original.domain());
+    for (int mode = 0; mode < original.modes(); ++mode) {
+        for (int k = radius_; k < original.samples() - radius_; ++k) {
+            for (int m = -radius_; m <= radius_; ++m) {
+                int borders = radius_ - std::abs(m);
+                for (int n = -borders; n <= borders; ++n) {
+                    equalized(mode, k) += weights_(m + radius_, n + radius_) *
+                                          term(original, mode, k, m, n);
+                }
+            }
+        }
     }
 
     return equalized;
 }
+
+void SINO::save_matrix(const std::string& filename) {
+    std::ofstream fout(filename);
+    save_matrix(fout);
+    fout.close();
+}
+
+void SINO::save_matrix(std::ostream& output) {
+    assert(trained_);
+
+    for (int m = 0; m < 2 * radius_ + 1; ++m) {
+        for (int n = 0; n < 2 * radius_ + 1; ++n) {
+            output << weights_(m, n).real() << ' ';
+        }
+        output << '\n';
+    }
+
+    output << "\n\n";
+    for (int m = 0; m < 2 * radius_ + 1; ++m) {
+        for (int n = 0; n < 2 * radius_ + 1; ++n) {
+            output << weights_(m, n).imag() << ' ';
+        }
+        output << '\n';
+    }
+
+    output << "\n\n";
+    for (int m = 0; m < 2 * radius_ + 1; ++m) {
+        for (int n = 0; n < 2 * radius_ + 1; ++n) {
+            output << std::abs(weights_(m, n)) << ' ';
+        }
+        output << '\n';
+    }
+
+    output << "\n\n";
+    for (int m = 0; m < 2 * radius_ + 1; ++m) {
+        for (int n = 0; n < 2 * radius_ + 1; ++n) {
+            output << std::arg(weights_(m, n)) << ' ';
+        }
+        output << '\n';
+    }
+}
+}  // namespace focss
--- src/focss/equalizer/sino.h
+++ src/focss/equalizer/sino.h
@@ -1,33 +1,53 @@
-#ifndef SINO_H_
-#define SINO_H_
+#ifndef FOCSS_EQUALIZER_SINO_H_
+#define FOCSS_EQUALIZER_SINO_H_
 
+#include <armadillo>
 #include "focss/field.h"
-#include "focss/utility.h"
+
+namespace focss {
+enum SinoType { ORDINARY, FIXED, HOMOGENEOUS };
 
 class SINO {
-    unsigned long radius;
-    double lambda;
-    bool trained;
-
-    Complex intercept;
-    ComplexVector weights;
-
   public:
     SINO();
-    SINO(const unsigned long& symbol_radius);
-    SINO(const unsigned long& symbol_radius, const double& parameter);
-    void setSymbolRadius(const unsigned long& symbol_radius);
-    void setRegularizationParameter(const double& parameter);
+    SINO(const int& radius);
+    SINO(const int& radius, const double& lambda);
+    SINO(const int& radius, const double& lambda, const SinoType& type);
+    SINO& set_radius(const int& radius);
+    SINO& set_lambda(const double& lambda);
+    SINO& set_type(const SinoType& type);
+    SINO& set_linear_weight(const complex_t& linear_weight);
 
-    void train(const Field& desired, const Field& actual);
+    void train(const Field& transmitted, const Field& received);
 
-    ComplexVector getWeights() const;
-    Field equalize(const Field& original) const;
+    complex_t get_intercept() const;
+    complex_t get_linear_weight() const;
+    complex_t get_coo_weight() const;
+    Field model(const Field& original) const;
+    Field equalize(const Field& rx, const Field& tx) const;
+    Field linear(const Field& original) const;
+    Field cubic(const Field& original) const;
+
+    void save_matrix(const std::string& filename);
+    void save_matrix(std::ostream& output);
 
   private:
-    Complex term(const Field& field,
-                 const unsigned long& center_index,
-                 const unsigned long& term_index) const;
-};
+    static complex_t term(const Field& field,
+                          const int& mode,
+                          const int& k,
+                          const int& m,
+                          const int& n);
 
-#endif  // SINO_H_
+  private:
+    int radius_;
+    double lambda_;
+    SinoType type_;
+    bool trained_;
+
+    complex_t intercept_;
+    complex_t linear_weight_;
+    arma::cx_mat weights_;
+};
+}  // namespace focss
+
+#endif  // FOCSS_EQUALIZER_SINO_H_
